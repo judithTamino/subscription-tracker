0e53f2412a2d67e84f77fe924415cf0a5cf3a2a9
יהודית טמינו <taminojudit@hotmail.co.il>
2025-05-31 22:58:05 +0300

    sign-in and sign-up authentication

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,17 +1,12 @@
 import { Router } from "express";
+import { signIn, signOut, signUp } from "../controllers/auth.controller.js";
 
 const authRouter = Router();
 
-authRouter.post("/sign-up", (req, res) => {
-  res.send({msg: "Sign up"});
-});
+authRouter.post("/sign-up", signUp);
 
-authRouter.post("/sign-in", (req, res) => {
-  res.send({msg: "Sign in"});
-});
+authRouter.post("/sign-in", signIn);
 
-authRouter.post("/sign-out", (req, res) => {
-  res.send({msg: "Sign out"});
-});
+authRouter.post("/sign-out", signOut);
 
 export default authRouter;
